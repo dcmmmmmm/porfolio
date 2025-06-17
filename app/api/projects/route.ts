23f634849380aeeadb37e25f6f8860864da6a05c
dcmmmmmm <ptson.son@gmail.com>
2025-06-18 00:14:30 +0700

    fix deploy on Vercel

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -3,7 +3,7 @@ import { NextResponse, NextRequest } from "next/server";
 
 export const dynamic = 'force-dynamic'
 
-export async function GET() {
+export async function GET(request: NextRequest) {
   try {
     const projects = await prisma.project.findMany({
       orderBy: {
